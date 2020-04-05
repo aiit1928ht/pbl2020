39f48022b2c7699cd5613e6b294403c01152fd7c
Hiroshi Tokuyama in AIIT <a1928ht@aiit.ac.jp>
2020-04-06 02:41:04 +0900

    make classes, FizzBuzz and FizzBuzzTest

---
+++ FizzBuzz/src/test/java/FizzBuzzTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+
+    @org.junit.jupiter.api.Test
+    void paramIsDivBy3() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void paramIsDivBy5() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void paramIsDivBy3and5() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void paramIsNotDivBy3and5() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void paramIsMinus() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void zeroDiv() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void paramIsNull() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void paramIsString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void paramIsLong() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void paramIsFloat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void paramIsBignum() {
+    }
+}
